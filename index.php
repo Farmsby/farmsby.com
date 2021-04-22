26dc882d9e1078883fce6de7399910dd80e753d7
Precious Tom <tomprezine@gmail.com>
2021-04-22 05:51:04 +0100

    push structure

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmsby | Africa's Most Lucrative Agro Investment Platform</title>
+    <link rel="stylesheet" href="./assets/libraries/bootstrap5/css/bootstrap.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/custom.css">
+</head>
+<body>
+    <!-- Visible Content: Start -->    
+    <header>
+        <div class="logo">
+            <img src="./assets/images/logo.png" alt="Farmsby Logo" class="w-10">
+        </div>
+        <nav>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">Company</a></li>
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Learn</a></li>
+            <li><a href="#">Get Help</a></li>
+            <li><a href="#" class="__btn">Sign In</a></li>
+        </nav>
+    </header>
+
+    <section>
+        
+    </section>
+    <!-- Visible Content: End -->    
+    
+    <script src="./assets/js/jquery.js"></script>
+    <script src="./assets/js/config.js"></script>
+    <script src="./assets/js/app.js"></script>
+    <script src="./assets/libraries/bootstrap5/js/bootstrap.js"></script>
+</body>
+</html>
