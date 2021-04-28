267d6bba5307d1a22d7cb0c8e1b5bb9b54553689
Precious Tom <tomprezine@gmail.com>
2021-04-28 19:45:40 +0100

    updated index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="./assets/libraries/fontawesome/css/all.css">
     <link rel="stylesheet" href="./assets/css/main.css">
     <link rel="stylesheet" href="./assets/css/custom.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Secular+One&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- Visible Content: Start -->    
@@ -37,18 +39,29 @@
     <img src="./assets/images/extras/flower.png" alt="Floor Hero" class="flower-scroller">
         <div class="container">
             <div class="row">
+                <div class="col-md-6 offset-md-3 text-center">
+                    <p class="text-white info">Welcome to Farmsby</p>
+                    <h2 class="text-white">
+                    To transition Aussie farmers to regenerative agriculture and support them to 
+                    rebuild the carbon (organic matter) in their soil from 1% to 8%. 
+                    </h2>
+                </div>
+            </div>
+            <div class="row pushdown-x2">
                 <div class="col-md-4">
                     <div class="icon_holder">
                         <div class="icon_circle">
 
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="icon_holder">
-                        <div class="icon_circle">
-                            
-                        </div>
+                    <div class="info-container">
+                        <h4 class="text-white">Energy Decentralization</h4>
+                        <p class="info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Ipsam at id doloribus cum, animi blanditiis minima, laudantium 
+                            nostrum veniam eligendi ad tenetur, non optio facere magnam 
+                            quasi harum! Aliquam, enim?
+                        </p>
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -56,6 +69,31 @@
                         <div class="icon_circle">
                             
                         </div>
+                    </div>
+                    <div class="info-container">
+                        <h4 class="text-white">Energy Decentralization</h4>
+                        <p class="info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Ipsam at id doloribus cum, animi blanditiis minima, laudantium 
+                            nostrum veniam eligendi ad tenetur, non optio facere magnam 
+                            quasi harum! Aliquam, enim?
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon_holder">
+                        <div class="icon_circle">
+                            
+                        </div>
+                    </div>
+                    <div class="info-container">
+                        <h4 class="text-white">Energy Decentralization</h4>
+                        <p class="info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Ipsam at id doloribus cum, animi blanditiis minima, laudantium 
+                            nostrum veniam eligendi ad tenetur, non optio facere magnam 
+                            quasi harum! Aliquam, enim?
+                        </p>
                     </div>
                 </div>
             </div>
