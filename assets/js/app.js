a6042b257a63dac162777197e70bcf37f0d710e4
Elizabeth Onyenekwe <drama_queen@Elizabeths-MacBook-Air.local>
2021-04-30 07:00:41 -0700

    motion

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,20 @@ $(window).scroll(function() {
         $("header.ps-fixed").removeClass("transparent");
     }
 });
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".images-container img");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  duration: 4,
+  ease: "linear",
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: () => innerHeight * 4,
+    scrub: true,
+    // pin: ".grid",
+    anticipatePin: 1
+  }
+})
