f150b7648d8d403c54c96241cb77ae95fccfef00
Elizabeth Onyenekwe <drama_queen@Elizabeths-MacBook-Air.local>
2021-04-30 11:44:56 -0700

    motion

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,18 @@ $(window).scroll(function() {
         $("header.ps-fixed").removeClass("transparent");
     }
 });
+
+
+// $(window).scroll(function() {    
+//   var scroll = $(window).scrollTop();
+//   if (scroll >= 1400) {
+//       $("__buttons").addClass("active");
+//   } else {
+//       $("__buttons").removeClass("active");
+//   }
+// });
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".images-container img");
@@ -23,3 +35,36 @@ gsap.to(sections, {
     anticipatePin: 1
   }
 })
+
+let cardsSections = gsap.utils.toArray(".card-view");
+
+gsap.to(cardsSections, {
+  xPercent: -100 * (cardsSections.length - 1),
+  duration: 2,
+  ease: "linear",
+  scrollTrigger: {
+    trigger: ".pushdown-x2 ul",
+    start: "top top",
+    end: () => innerHeight * 4,
+    scrub: true,
+    pin: ".carholder",
+    anticipatePin: 1
+  }
+})
+
+
+// let cardsButtons = gsap(".__buttons");
+
+// gsap.to(cardsButtons, {
+//   position: "fixed",
+//   duration: 2,
+//   ease: "linear",
+//   scrollTrigger: {
+//     trigger: ".pushdown-x2 ul",
+//     start: "top top",
+//     end: () => innerHeight * 4,
+//     scrub: true,
+//     pin: ".carholder",
+//     anticipatePin: 1
+//   }
+// })
