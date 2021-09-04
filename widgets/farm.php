eb9697d328ca3e9b2cc3a940ce20d7df36fc94fa
Precious Tom <tomprezine@gmail.com>
2021-09-04 11:17:57 +0100

    Update

--- widgets/farm.php
+++ widgets/farm.php
@@ -9,8 +9,11 @@
                 Experts at Farmsby would be willing to help. Talk to our experts now by Scheduling a meeting or give us a quick call today.
                 </h3>
                 <ul class="global-signup__call-to-action">
-                    <li class="global-signup__button">
+                    <!-- <li class="global-signup__button">
                         <a class="button button--green" href="https://calendly.com/farmsby">Book a meeting</a>
+                    </li> -->
+                    <li class="global-signup__button">
+                        <a class="button button--green" href="https://bit.ly/farmsby-farm">Get Started</a>
                     </li>
                     <li class="global-signup__button">
                         <a class="button button--default" href="tel:+2348065032176">Call us</a>
